d7bfbfbbd851fe5584b0bfddd364f1b2e05117b7
flj <209319393@qq.com>
2026-08-17 15:28:23 +0800

    订单状态定时处理代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -55,15 +55,15 @@ public interface OrderMapper {
     @Select("select count(id) from orders where status = #{status}")
     Integer countStatus(Integer status);
 
-//    /**
-//     * 根据订单状态和下单时间查询订单
-//     * @param status
-//     * @param orderTime
-//     * @return
-//     */
-//    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
-//    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
-//
+    /**
+     * 根据订单状态和下单时间查询订单
+     * @param status
+     * @param orderTime
+     * @return
+     */
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
+
 //    /**
 //     * 根据动态条件统计营业额数据
 //     * @param map
